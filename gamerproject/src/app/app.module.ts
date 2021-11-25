8e3153cdc814b3ed9cb8d2184527f4565912117b
marcoslyra <markinhoskelly@outlook.com>
2021-11-24 22:53:25 -0300

    Teste 3

--- gamerproject/src/app/app.module.ts
+++ gamerproject/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
